54d983a061489ec32e120fc3b45960da108c0fef
Petru Bodea <petrubodea94@gmail.com>
2020-03-25 11:52:33 +0200

    message change

--- src/com/cakeshop/utils/Messages.java
+++ src/com/cakeshop/utils/Messages.java
@@ -6,7 +6,7 @@ public class Messages {
     public static final String INTRO_MESSAGE = "Please enter a number associated to the desired cake \n" +
             "You may type 'X' to quit.\n";
     public static final String IN_THE_RANGE_1_12_MESSAGE = "\nPlease enter a numeric value in the range 1-12\n";
-    public static final String ORDER_TO_A_MANAGER_MESSAGE = "\n Please stand by as we assign your order to a manager ... \n";
+    public static final String ORDER_TO_A_MANAGER_MESSAGE = "\n Please stand by as all managers are currently busy ... \n";
     public static final String SHUTTING_DOWN_EXECUTORS_MESSAGE = "Closing shop...";
     public static final String CAKE_ORDER_MESSAGE = "What cake would you like to order ?";
 
